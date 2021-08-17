3581332150adb51876727210f5a1f37ee47f6062
Fuad <kamilovfuad@gmail.com>
2021-08-17 21:01:04 +0400

    Do part of the merge_sort alghoritm

--- fibonacci.rb
+++ fibonacci.rb
@@ -5,7 +5,7 @@ def fibs(n)
   end
   arr
 end
-p fibs(10)
+# p fibs(10)
 
 def fibs_rec(n, arr = [0, 1])
   arr[arr.length] = (arr[arr.length - 1] + arr[arr.length - 2])
@@ -13,4 +13,25 @@ def fibs_rec(n, arr = [0, 1])
   arr
 end
 
-p fibs_rec(10)
+# p f1ibs_rec(10)
+
+a_arr = []
+b_arr = []
+
+def merge_sort(arr, new_arr = [], a_arr = [], b_arr = [])
+  if arr.length == 1
+    arr
+  elsif arr.length == 2
+    a = arr[0]
+    b = arr[-1]
+    arr[0], arr[-1] = arr[-1], arr[0] if a > b
+    arr
+  else
+    arr = a = merge_sort(arr.slice!(0, arr.length/2)) + b = merge_sort(arr)
+  end
+
+  arr
+  
+end
+
+p merge_sort([1, 7, 2, 6, 5, 8])
