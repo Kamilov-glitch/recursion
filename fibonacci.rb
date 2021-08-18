20c5bf2d1b9da1b6883c044c3808c0acac80a748
Fuad <kamilovfuad@gmail.com>
2021-08-18 22:05:09 +0400

    Complete self-made merge_sort alghoritm

--- fibonacci.rb
+++ fibonacci.rb
@@ -18,20 +18,34 @@ end
 a_arr = []
 b_arr = []
 
-def merge_sort(arr, new_arr = [], a_arr = [], b_arr = [])
+def merge_sort(arr, new_arr = [])
+  
   if arr.length == 1
-    arr
+    return arr
   elsif arr.length == 2
     a = arr[0]
     b = arr[-1]
     arr[0], arr[-1] = arr[-1], arr[0] if a > b
-    arr
+    return arr
   else
-    arr = a = merge_sort(arr.slice!(0, arr.length/2)) + b = merge_sort(arr)
+    a = merge_sort(arr.slice!(0, arr.length/2)) 
+    b = merge_sort(arr)
   end
 
-  arr
+  until a.length == 0 || b.length == 0
+    if a[0] < b[0]
+      new_arr.push a[0] 
+      a.delete_at(0)
+    else
+      new_arr.push b[0]
+      b.delete_at(0)
+    end
+  end
+
+  new_arr += (a.length > 0 ? a : b)
+
+  arr = new_arr
   
 end
 
-p merge_sort([1, 7, 2, 6, 5, 8])
+p merge_sort([8, 1, 6, 2, 5, 9, 7, 6, 6, 6])
